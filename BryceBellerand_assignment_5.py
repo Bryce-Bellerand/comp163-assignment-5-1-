ea0baebc84e82a712dbb1ae663a43271ae4ee1b1
Bryce-Bellerand <brycebellerand@gmail.com>
2025-10-03 22:54:35 -0400

    git commit -m "Challenge 2: Prime checker - for loop for known range"

--- BryceBellerand_assignment_5.py
+++ BryceBellerand_assignment_5.py
@@ -16,3 +16,20 @@ else:
     print(current_number)
 
 print(f"Steps: {step_count}")
+print("=== Challenge 2: Prime Number Checker ===")
+prime_user = int(input())
+print(f"Enter a number: Testing divisors from 2 to {prime_user - 1}...")
+
+for i in range(2,17): 
+    if prime_user % i == 0:
+        prime_number = False
+        break
+    else:
+        prime_number = True
+if prime_user == 1 or prime_user == 2:
+    prime_number = True
+
+if prime_number == True:
+    print(f"{prime_user} is prime!")
+else:
+    print(f"{prime_user} is not prime (divisible by {i})")
